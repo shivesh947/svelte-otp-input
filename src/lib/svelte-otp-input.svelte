455ec27a161fa7e1d2a038ba0b6325724f9cb869
Shivesh Gupta <guptashiv84@gmail.com>
2023-06-01 12:46:46 +0530

    minor fixes

--- src/lib/svelte-otp-input.svelte
+++ src/lib/svelte-otp-input.svelte
@@ -1,21 +1,19 @@
 <script lang="ts" type="ts">
-  import { onMount } from "svelte";
+  import { onMount, onDestroy } from "svelte";
   import "./main.css";
 
+  export let wrapperStyle = "";
+  export let inputStyle = "";
   let otpNumber: string[] = [];
+
+  let inputEleList: Array<HTMLInputElement> = [];
 
   export let setOtp: (val: string, paste?: boolean) => void = () => {};
   export let otpCount: number = 6;
-  export let wrapperStyle = "";
-  export let inputStyle = "";
 
   const handleInput = (index: number, value: string) => {
-    const eleNext = document.getElementById(
-      "onramp_login_otp" + (index + 1)
-    ) as HTMLInputElement;
-    const eleCurr = document.getElementById(
-      "onramp_login_otp" + index
-    ) as HTMLInputElement;
+    const eleNext = inputEleList[index + 1];
+    const eleCurr = inputEleList[index];
     value = value.replace(/[^1-9]/g, "");
     if (eleCurr) {
       eleCurr.value = value;
@@ -23,55 +21,49 @@
     otpNumber[index] = value;
     setOtp(otpNumber.join(""));
 
-    if (value !== "") {
-      if (index < otpCount && eleNext) {
-        eleNext.focus();
-      }
+    if (value !== "" && index < otpCount && eleNext) {
+      eleNext.focus();
     }
   };
 
   const handleKeyDown = (index: number, event: KeyboardEvent) => {
-    const elePrev = document.getElementById("onramp_login_otp" + (index - 1));
-    const eleNext = document.getElementById("onramp_login_otp" + (index + 1));
-    if (event.key === "ArrowRight" || event.key === "Tab") {
-      event.preventDefault();
-      if (index < otpCount && eleNext) {
-        eleNext.focus();
-      }
-    } else if (event.key === "ArrowLeft") {
-      event.preventDefault();
-      if (index > 1 && elePrev) {
-        elePrev.focus();
-      }
-    } else if (
-      event.key === "Backspace" &&
-      document.getElementById("onramp_login_otp" + index)?.value === ""
+    const elePrev = inputEleList[index - 1];
+    const eleNext = inputEleList[index + 1];
+
+    if (
+      (event.key === "ArrowRight" || event.key === "Tab") &&
+      index < otpCount &&
+      eleNext
     ) {
       event.preventDefault();
-      if (index > 1 && elePrev) {
-        elePrev.focus();
-      }
+      eleNext.focus();
+    } else if (event.key === "ArrowLeft" && index > 0 && elePrev) {
+      event.preventDefault();
+      elePrev.focus();
+    } else if (
+      event.key === "Backspace" &&
+      inputEleList[index]?.value === "" &&
+      index > 0 &&
+      elePrev
+    ) {
+      event.preventDefault();
+      elePrev.focus();
     } else if (event.key === "v" && event.ctrlKey) {
-      // Handle paste event
       handlePaste(event);
     }
   };
 
-  const handlePaste = (event: KeyboardEvent) => {
+  const handlePaste = (event: Event) => {
     event.preventDefault();
     const clipboardData = event?.clipboardData || window?.clipboardData;
     const pastedData = clipboardData?.getData("text");
-    const otpInputs = Array.from(
-      document.querySelectorAll("input[type='text']")
-    ) as HTMLInputElement[];
-
-    for (let i = 0; i < otpInputs.length; i++) {
+    for (let i = 0; i < inputEleList.length; i++) {
       if (pastedData[i] && i < otpCount) {
         const value = pastedData[i].replace(/[^1-9]/g, "");
-        otpInputs[i].value = value;
+        inputEleList[i].value = value;
         otpNumber[i] = value;
         if (i < 5) {
-          otpInputs[i + 1].focus();
+          inputEleList[i + 1].focus();
         }
       }
     }
@@ -80,42 +72,35 @@
 
   onMount(() => {
     setTimeout(() => {
-      const otpInputFields = Array.from(
-        document.querySelectorAll("input[type='text']")
-      ) as HTMLInputElement[];
-      if (otpInputFields.length > 0) {
-        otpInputFields.forEach((input) => {
+      if (inputEleList.length > 0) {
+        inputEleList.forEach((input) => {
           input.addEventListener("paste", handlePaste);
         });
-        otpInputFields[0].focus();
+        inputEleList[0].focus();
       }
     }, 100);
   });
 
-//   onDestroy(() => {
-//     const otpInputFields = Array.from(
-//       document.querySelectorAll("input[type='text']")
-//     ) as HTMLInputElement[];
-//     if (otpInputFields.length > 0) {
-//       otpInputFields.forEach((input) => {
-//         input.removeEventListener("paste", handlePaste);
-//       });
-//     }
-//   });
-
+  onDestroy(() => {
+    if (inputEleList.length > 0) {
+      inputEleList.forEach((input) => {
+        input.removeEventListener("paste", handlePaste);
+      });
+    }
+  });
   const countArray = new Array(otpCount);
 </script>
 
 <div class="otp_input_wrapper" style={wrapperStyle}>
   {#each countArray as i, inputCount}
     <input
+      style={inputStyle}
+      bind:this={inputEleList[inputCount]}
       type="text"
-      id={`onramp_login_otp${inputCount + 1}`}
       class="otp_input"
       maxlength="1"
-      style={inputStyle}
-      on:input={(e) => handleInput(inputCount + 1, e.currentTarget.value)}
-      on:keydown={(e) => handleKeyDown(inputCount + 1, e)}
+      on:input={(e) => handleInput(inputCount, e.currentTarget.value)}
+      on:keydown={(e) => handleKeyDown(inputCount, e)}
     />
   {/each}
 </div>
